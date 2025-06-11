6ad3cfa0026f8d62d4300c5dfbbe6e911b53e9c2
arlasaikiran143 <arlasaikiran143@gmail.com>
2025-06-11 17:41:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,25 @@
-resource "local_file" "my_animal"{
-    filename = "pets.txt"
-    content = "I love my cat"
-    lifecycle {
-      create_before_destroy = true
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
     }
-    }
+  }
+}
+
+provider "random" {}
 
 resource "random_pet" "petname" {
-  prefix = "MR"
+  prefix    = "MR"
   separator = "."
-length = "1"
+  length    = 1
+}
+
+resource "local_file" "my_animal" {
+  filename = "pets.txt"
+  content  = "I love my cat"
+  
+  lifecycle {
+    create_before_destroy = true
+  }
 }
